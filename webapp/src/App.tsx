930398731b30efa2417830620f925648c3b744d7
Ubuntu <rocky-bana@rockyvm.zlluyxpu2pouvindzvffqxcmxe.rx.internal.cloudapp.net>
2025-06-15 15:47:25 +0000

    format

---
+++ webapp/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+<div className="flex flex-col w-full min-h-lvh ...">     
+
+<div id="header" className="border-2 border-solid w-full sticky top-0">   Header  </div>
+<div id="content" className="border-2 border-solid w-full min-h-lvh">  Content   </div>
+
+<div id="footer" className="border-2 border-solid w-full sticky bottom-0">  Footer   </div>
+</div>)
+}
+
+export default App
